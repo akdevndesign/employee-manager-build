513487ef53f3fa333383113b69d0dca1ac53db1b
SoftPoachedEggs <115499632+SoftPoachedEggs@users.noreply.github.com>
2023-03-13 19:42:30 -0400

    added async functions for inquirer prompts

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,8 @@ CREATE TABLE emp_role (
   role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   role_title VARCHAR(30) NOT NULL,
   role_salary DECIMAL,
-  department_id INT
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(department_id)
 );
 
 CREATE TABLE employees (
@@ -20,10 +21,7 @@ CREATE TABLE employees (
   emp_first_name VARCHAR(30) NOT NULL,
   emp_last_name VARCHAR(30) NOT NULL,
   role_id INT,
-  manager_id INT
-  );
-
-
-
-
-
+  manager_id INT NULL,
+  FOREIGN KEY (role_id) REFERENCES emp_role(role_id),
+  FOREIGN KEY (manager_id) REFERENCES employees(employee_id)
+);
--- script.js
+++ script.js
@@ -13,46 +13,52 @@ async function init() {
       type: 'list',
       choices: ['View all Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Deptartment', 'Exit'],
       message: 'What would you like to do?'
-  }, 
-  {
-    
-
   });
 
-switch (choice.welcomePrompt){
-  case 'View all Employees':
-    viewAllEmployees()
-    break;
-  case 'Add Employee':
-    addEmployee()
-    break;
-  case 'Update Employee Role':
-    updateEmployeeRole()
-    break;
-  case 'View All Roles':
-    viewAllRoles()
-    break;
-  case 'Add Role':
-    addRole()
-    break;
-  case 'View All Departments':
-    viewAllDepartments();
-    break;
-  case 'Add Deptartment':
-    addDeptartment()
-    break;
-  case 'Exit':
-    exitApp();
-    break;
-  };
-
+  switch (choice.welcomePrompt){
+    case 'View all Employees':
+      viewAllEmployees()
+      break;
+    case 'Add Employee':
+      addEmployee()
+      break;
+    case 'Update Employee Role':
+      updateEmployeeRole()
+      break;
+    case 'View All Roles':
+      viewAllRoles()
+      break;
+    case 'Add Role':
+      addRole()
+      break;
+    case 'View All Departments':
+      viewAllDepartments();
+      break;
+    case 'Add Deptartment':
+      addDeptartment()
+      break;
+    case 'Exit':
+      exitApp();
+      break;
+    };
 };
+
   //View all employees function
 
   //employee_id, employee_first, employee_last, employee.role_id = emp_role.role_title and (department_id = department.department_name)
   //and salary, manager = concat employee.employee_first & employee.employee_last
   async function viewAllEmployees(){
-    db.query(`SELECT employee.employee_id`,
+    db.query(`SELECT 
+    employees.employee_id, 
+    employees.emp_first_name, 
+    employees.emp_last_name, 
+    emp_role.role_title, 
+    department.department_name, 
+    emp_role.role_salary, 
+    CONCAT(managers.emp_first_name, ' ', managers.emp_last_name) AS manager_name FROM employees 
+    JOIN emp_role ON employees.role_id = emp_role.role_id
+    JOIN department ON emp_role.department_id = department.department_id
+    LEFT JOIN employees AS managers ON employees.manager_id = managers.employee_id;`,
     function (err, results){
       console.log(`\n`);
       console.table(results);
@@ -60,42 +66,161 @@ switch (choice.welcomePrompt){
       init();
     })
   };
-  //Add Employee function
-  function addEmployee(){
+  // //Add Employee function
+  async function addEmployee() {
+    const addEmployeeQuestions = [
+      {
+        type: 'input',
+        message: 'What is the first name of the employee?',
+        name: 'newFirstName',
+        validate: validateName
+      },
+      {
+        type: 'input',
+        message: 'What is the last name of the employee?',
+        name: 'newLastName',
+        validate: validateName
+      },
+      {
+        type: 'list',
+        message: 'What is the role for the employee?',
+        name: 'newEmpRole',
+        choices: await roleChoices(),
+      },
+      {
+        type: 'list',
+        message: 'Who is their manager?',
+        name: 'newEmpManager',
+        choices: await managerChoices(),
+      }
+    ];
+    
+    let employeeInfo = await inquirer.prompt(addEmployeeQuestions);
 
-  };
-      
-  //Update Employee Role
-  function addRole(){
-    const query = 'SELECT department_id AS value, department_name AS name FROM department';
-    db.query(query, (err, results) => {
+  // Look up the employee ID associated with the selected full name
+  const selectedManager = employeeInfo.newEmpManager;
+  const managerQuery = `SELECT employee_id FROM employees WHERE CONCAT(emp_first_name, ' ', emp_last_name) = '${selectedManager}'`;
+  db.query(managerQuery, (err, managerResults) => {
     if (err) throw err;
-    inquirer.prompt([{
-    type: 'input',
-    message: 'What is the title of the role?',
-    name: 'newRole',
-    validate: validateName
-  }, 
-  {
-    type: 'input',
-    message: 'What is the salary of the role?',
-    name: 'newRoleSalary',
-    validate: validateNumber
-  }, 
-  {
-    type: 'list',
-    message: 'What is the department for the role?',
-    name: 'newRoleDept',
-    choices: results,
-    validate: validateNumber
-  }
-]).then((function(res){
-    db.query("INSERT INTO emp_role SET ?", {role_title: res.newRole, role_salary: res.newRoleSalary, department_id: res.newRoleDeptID})
-    console.log(`\n Role successfully entered.\n`);
-    init();
-  }))
-});
+  
+  const managerId = managerResults[0].employee_id;
+
+
+  db.query(
+        'INSERT INTO employees SET ?',
+        {
+          emp_first_name: employeeInfo.newFirstName,
+          emp_last_name: employeeInfo.newLastName,
+          role_id: employeeInfo.newEmpRole,
+          manager_id: managerId
+        },
+        (err, res) => {
+          if (err) throw err;
+          console.log(`\n Role successfully entered.\n`);
+          init();
+        }
+      );
+      });
+
+      async function managerChoices() {
+        return new Promise((resolve, reject) => {
+          const query = 'SELECT CONCAT(emp_first_name, " ", emp_last_name) AS manager_name FROM employees';
+          db.query(query, (err, results) => {
+            if (err) reject(err);
+            resolve(results.map(result => result.manager_name));
+          });
+        });
+      };
+    };
+
+  async function roleChoices() {
+    return new Promise((resolve, reject) => {
+      const query = 'SELECT role_title FROM emp_role';
+      db.query(query, (err, results) => {
+        if (err) reject(err);
+        resolve(results.map(result => result.role_title));
+      });
+    });
   };
+
+  //Update Employee Role
+  async function addEmployee() {
+    const addEmployeeQuestions = [
+      {
+        type: 'input',
+        message: 'What is the first name of the employee?',
+        name: 'newFirstName',
+        validate: validateName
+      },
+      {
+        type: 'input',
+        message: 'What is the last name of the employee?',
+        name: 'newLastName',
+        validate: validateName
+      },
+      {
+        type: 'list',
+        message: 'What is the role for the employee?',
+        name: 'newEmpRole',
+        choices: await roleChoices(),
+      },
+      {
+        type: 'list',
+        message: 'Who is their manager?',
+        name: 'newEmpManager',
+        choices: await managerChoices(),
+      }
+    ];
+  
+    let employeeInfo = await inquirer.prompt(addEmployeeQuestions);
+  
+    // Look up the employee ID associated with the selected full name
+    const selectedManager = employeeInfo.newEmpManager;
+    const managerQuery = `SELECT employee_id FROM employees WHERE CONCAT(emp_first_name, ' ', emp_last_name) = '${selectedManager}'`;
+    
+    db.query(managerQuery, (err, managerResults) => {
+      if (err) throw err;
+      const managerId = managerResults[0].employee_id;
+  
+      // Insert the new employee with the manager ID
+      db.query(
+        'INSERT INTO employees SET ?',
+        {
+          emp_first_name: employeeInfo.newFirstName,
+          emp_last_name: employeeInfo.newLastName,
+          role_id: employeeInfo.newEmpRole,
+          manager_id: managerId
+        },
+        (err, res) => {
+          if (err) throw err;
+          console.log(`\n Employee successfully saved.\n`);
+          init();
+        }
+      );
+    });
+
+  //query manager names and concat to one name. Then save manager as employee ID
+    async function managerChoices() {
+      return new Promise((resolve, reject) => {
+        const query = 'SELECT CONCAT(emp_first_name, " ", emp_last_name) AS manager_name FROM employees';
+        db.query(query, (err, results) => {
+          if (err) reject(err);
+          resolve(results.map(result => result.manager_name));
+        });
+      });
+    };
+  };
+  
+  //query role titles and save as role_id
+  async function roleChoices() {
+    return new Promise((resolve, reject) => {
+      const query = 'SELECT role_id, role_title FROM emp_role';
+      db.query(query, (err, results) => {
+        if (err) reject(err);
+        resolve(results.map(result => ({ name: result.role_title, value: result.role_id })));
+      });
+    });
+  }
 
   //view all departments function
 function viewAllDepartments(){
@@ -107,17 +232,16 @@ function viewAllDepartments(){
   };
 
   //view all roles function
- function viewAllRoles(){
-    db.query(`SELECT * FROM emp_role`, function (err, results){
+function viewAllRoles(){
+    db.query(`SELECT emp_role.role_id, emp_role.role_title, emp_role.role_salary, department.department_name, department.department_id FROM emp_role JOIN department ON emp_role.department_id = department.department_id ORDER BY emp_role.role_id ASC`, function (err, results){
       console.log(`\n`);
       console.table(results)
       init();
     })
   };
 
-
-  
-  function addDeptartment(){
+//ADD DEPARTMENT FUNCTION
+function addDeptartment(){
     inquirer.prompt({
         type: 'input',
         message: 'What is the name of the department?',
@@ -125,7 +249,10 @@ function viewAllDepartments(){
         validate: validateString
       })
       .then((function(res){
-        db.query("INSERT INTO department SET ?", {department_name: res.newDept})
+        db.query("INSERT INTO department SET ?", 
+        {
+          department_name: res.newDept
+        })
         console.log(`\n Department successfully entered.\n`);
         init();
       }))
@@ -137,39 +264,7 @@ let exitApp = () => {
   process.exit(console.log("\nGoodbye!"));
 }
 
-  //   let employeeProfile = await inquirer.prompt(employeeQuestions);
-    
-  //   switch (employeeProfile.role) {
-  //     case 'Engineer':
-  //       let addEngineer = new Engineer(employeeProfile.name, employeeProfile.id, employeeProfile.email, employeeProfile.gitname)
-  //       employeeData.push(addEngineer);
-  //       break;
-  //     case 'Manager':
-  //       let addManager = new Manager(employeeProfile.name, employeeProfile.id, employeeProfile.email, employeeProfile.officenumber)
-  //       employeeData.push(addManager);
-  //       break;
-  //     case 'Intern':
-  //       let addIntern = new Intern(employeeProfile.name, employeeProfile.id, employeeProfile.email, employeeProfile.school)
-  //       employeeData.push(addIntern);
-  //       break;
-  //     default: 
-  //       writeToFile();
-        
-
-
-
 init();
-
-
-
-// // Function call to initialize app
-// init();
-
-// //create array for employee data
-// const employeeData = [];
-// console.log('Employee data:', employeeData)
-
-// const roles = ['Manager','Engineer', 'Intern']
 
 //~~~~~~~~~~~Validate functions~~~~~~~~~~~~~~~
 // //create input validation checks for prompt questions
@@ -308,5 +403,4 @@ const validateString = (input) => {
 //   fs.writeFileSync(fileName, generateHTML(employeeData))
 //   console.log('Employee data:', employeeData)
 //   console.log("File created successfully!");
-// }
-
+//
